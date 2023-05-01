b40298e0f7a70e2aa91c280f74ab575d51c38b21
mahmood2002reda <111697516+mahmood2002reda@users.noreply.github.com>
2023-05-01 15:36:23 +0300

    Add files via upload

---
+++ DataVisualization Chart/DataVisualization Chart/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace DataVisualization_Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.plot = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.comboBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.plot);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.chart1);
+            this.splitContainer1.Size = new System.Drawing.Size(553, 391);
+            this.splitContainer1.SplitterDistance = 57;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(361, 16);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.Text = "Select Chart Type";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // plot
+            // 
+            this.plot.Location = new System.Drawing.Point(216, 16);
+            this.plot.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.plot.Name = "plot";
+            this.plot.Size = new System.Drawing.Size(100, 28);
+            this.plot.TabIndex = 0;
+            this.plot.Text = "Plot";
+            this.plot.UseVisualStyleBackColor = true;
+            this.plot.Click += new System.EventHandler(this.plot_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chart1.Name = "chart1";
+            this.chart1.Size = new System.Drawing.Size(553, 329);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(553, 391);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button plot;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ DataVisualization Chart/DataVisualization Chart/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace DataVisualization_Chart
+{
+    public partial class Form1 : Form
+    {
+        string name = "myData";
+        List<double> x = new List<double>();
+        List<double> y = new List<double>();
+        public Form1()
+        {
+            InitializeComponent();
+            string[] charttype = new string[] { "Area", "Bar", "Bubble", "Column", "Doughnut", "Line", "Point", "Funnel",
+                "RangeBar", "Spline", "SplineArea", "StepLine" };
+            comboBox1.Items.AddRange(charttype);
+            for (int i = 0; i < 21; i++)
+            {
+                x.Add(i - 10);
+                y.Add(Math.Pow((i - 10), 2));
+            }
+        }
+
+        private void plot_Click(object sender, EventArgs e)
+        {
+            chart1.BackColor = Color.LightBlue;
+            chart1.Series.Add(name);
+            chart1.Series[name].Color = Color.DarkSlateGray;
+            // chart1.Series[name].Label = "Y = #"+Y_Label+"\nX = #"+X_Label;
+            chart1.ChartAreas[0].AxisX.TitleForeColor = Color.Gold;
+
+            chart1.Series[name].Points.DataBindXY(x, y);
+            chart1.Series[name].ChartArea = "ChartArea1";
+            chart1.Series[name].SetDefault(true);
+            chart1.Series[name].Enabled = true;
+            chart1.Visible = true;
+            chart1.Show();
+            comboBox1.Enabled = true;
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string s = comboBox1.SelectedItem.ToString();
+            switch (s)
+            {
+                case "Area":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Area;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+                case "Bar":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bar;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+                case "Bubble":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Bubble;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+                case "Column":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+                case "Doughnut":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Doughnut;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+                case "Line":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+                case "Point":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+                case "Funnel":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Funnel;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+                case "RangeBar":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.RangeBar;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    chart1.ChartAreas[0].AxisX.Maximum =x.Max();
+                    break;
+                case "Spline":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+                case "SplineArea":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.SplineArea;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+                case "StepLine":
+                    chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+                    chart1.ChartAreas[0].AxisX.Minimum = x.Min();;
+                    chart1.ChartAreas[0].AxisX.Interval = 1;
+                    break;
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
